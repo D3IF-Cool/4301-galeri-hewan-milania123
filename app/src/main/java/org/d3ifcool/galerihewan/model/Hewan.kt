53af451b22398eee1ef81b861ba74a97173ee1f3
nandaputrimilania <nandaputrimilania6@gmail.com>
2021-05-03 19:22:34 +0700

    Memproses data internet sebagai JSON

--- app/src/main/java/org/d3ifcool/galerihewan/model/Hewan.kt
+++ app/src/main/java/org/d3ifcool/galerihewan/model/Hewan.kt
@@ -1,8 +1,10 @@
 package org.d3ifcool.galerihewan.model
 
+import org.d3ifcool.galerihewan.R
+
 data class Hewan (
     val name: String,
     val nameLatin: String,
-    val imageResId: Int,
+    val imageResId: Int = R.drawable.angsa
 )
     
