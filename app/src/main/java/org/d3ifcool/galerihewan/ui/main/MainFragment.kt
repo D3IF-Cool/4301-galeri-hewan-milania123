0929daaf84798ea048b20ae8ec40b07ffe598270
nandaputrimilania <nandaputrimilania6@gmail.com>
2021-05-03 19:46:42 +0700

    Menambahkan loading indicator

--- app/src/main/java/org/d3ifcool/galerihewan/ui/main/MainFragment.kt
+++ app/src/main/java/org/d3ifcool/galerihewan/ui/main/MainFragment.kt
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import org.d3ifcool.galerihewan.R
 import org.d3ifcool.galerihewan.databinding.FragmentMainBinding
+import org.d3ifcool.galerihewan.network.ApiStatus
 
 class MainFragment : Fragment() {
 
@@ -45,5 +46,24 @@ class MainFragment : Fragment() {
         viewModel.getData().observe(viewLifecycleOwner, {
             myAdapter.updateData(it)
         })
+
+        viewModel.getStatus().observe(viewLifecycleOwner, {
+            updateProgress(it)
+        })
+    }
+
+    private fun updateProgress(status: ApiStatus) {
+        when (status) {
+            ApiStatus.LOADING -> {
+                binding.progressBar.visibility = View.VISIBLE
+            }
+            ApiStatus.SUCCESS -> {
+                binding.progressBar.visibility = View.GONE
+            }
+            ApiStatus.FAILED -> {
+                binding.progressBar.visibility = View.GONE
+                binding.networkError.visibility = View.VISIBLE
+            }
+        }
     }
 }
